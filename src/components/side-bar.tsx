51cffa653f329d31b09f69b7b2aaf44c13d9b588
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-09 04:49:39 +0530

    feat: simple sidebar
    Implement  a simple side bar instead of an accordion as committed earlier.

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -1,33 +1,13 @@
 "use client";
 
-import { fetchAlbums } from "@/actions/add-to-album";
-import { AccordionContent } from "@radix-ui/react-accordion";
 import { Folder, Heart, Image, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React, { useEffect, useState, useTransition } from "react";
-import { FetchAlbumsResultTypes, FolderType } from "../../types";
-import { Accordion, AccordionItem, AccordionTrigger } from "./ui/accordion";
+import React from "react";
 import { Button } from "./ui/button";
 
 const SideBar = () => {
   const pathname = usePathname();
-  const [albums, setAlbums] = useState<FolderType[]>();
-  const [isPending, startTransition] = useTransition();
-
-  useEffect(() => {
-    startTransition(async () => {
-      const result: FetchAlbumsResultTypes = await fetchAlbums();
-
-      if (!result) {
-        setAlbums([]);
-      }
-
-      setAlbums(result.folders);
-    });
-  }, []);
-
-  console.log(albums);
 
   return (
     <div className="pb-12 sm:w-1/3 max-w-[250px]">
@@ -53,42 +33,17 @@ const SideBar = () => {
                 <span className="hidden sm:block">Gallery</span>
               </Button>
             </Link>
-            {/* <Link href={"/albums"}> */}
-            <Accordion type="single" collapsible className="w-full">
-              <AccordionItem value="item-1" className="border-b-0">
-                <AccordionTrigger className="hover:no-underline hover:bg-accent rounded-md px-4 text-sm text-slate-400 hover:text-accent-foreground py-2">
-                  <div className="flex items-center">
-                    <Folder className="w-4 h-4 sm:mr-2" />
-                    <span className="hidden sm:block">Albums</span>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent>
-                  <div className="max-h-[250px] searchAlbumsContainer flex flex-col w-full overflow-auto">
-                    <Link href={"/albums"}>
-                      <Button
-                        variant={"ghost"}
-                        className="w-full flex justify-start pl-8 text-slate-400"
-                      >
-                        <Folder className="h-4 w-4 mr-2" />
-                        All albums
-                      </Button>
-                    </Link>
-                    {albums?.map((folder) => (
-                      <Link href={`/albums/${folder.name}`}>
-                        <Button
-                          variant={"ghost"}
-                          className="w-full flex justify-start pl-8 text-slate-400"
-                        >
-                          <Folder className="h-4 w-4 mr-2" />
-                          {folder.name}
-                        </Button>
-                      </Link>
-                    ))}
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-            {/* </Link> */}
+
+            <Link href={"/albums"}>
+              <Button
+                variant={pathname === "/albums" ? "secondary" : "ghost"}
+                className="sm:w-full justify-start w-fit text-slate-400"
+              >
+                <Folder className="w-4 h-4 sm:mr-2" />
+                <span className="hidden sm:block">Albums</span>
+              </Button>
+            </Link>
+
             <Link href={"/favorites"}>
               <Button
                 variant={pathname === "/favorites" ? "secondary" : "ghost"}
