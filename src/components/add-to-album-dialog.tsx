a75770a616a088167f1f42c236ec91a27e153bc0
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-09 14:44:32 +0530

    refactor: search album functionality
    Implement code splitting for search album inside the add-to-album-dialog component.

--- src/components/add-to-album-dialog.tsx
+++ src/components/add-to-album-dialog.tsx
@@ -11,11 +11,10 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Folder, FolderPlus } from "lucide-react";
-import { ChangeEvent, useEffect, useState, useTransition } from "react";
+import { FolderPlus, Loader2 } from "lucide-react";
+import { useEffect, useState, useTransition } from "react";
 import { FetchAlbumsResultTypes, FolderType, Image } from "../../types";
+import SearchAlbums from "./search-albums";
 
 type AddToAlbumProps = {
   image: Image;
@@ -27,12 +26,14 @@ export function AddToAlbumDialog({ image, onClose }: AddToAlbumProps) {
   const [isPending, startTransition] = useTransition();
   const [open, setOpen] = useState(false);
   const [albums, setAlbums] = useState<FolderType[]>();
-  const [searchAlbums, setSearchAlbums] = useState<FolderType[]>();
 
   const handleClick = async () => {
-    setOpen(false);
-    onClose();
-    await addToAlbumAction(albumName, image);
+    startTransition(async () => {
+      await addToAlbumAction(albumName, image).then(() => {
+        onClose();
+        setOpen(false);
+      });
+    });
   };
 
   useEffect(() => {
@@ -42,21 +43,6 @@ export function AddToAlbumDialog({ image, onClose }: AddToAlbumProps) {
       setAlbums(result.folders);
     });
   }, []);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setAlbumName(e.target.value);
-
-    const filteredAlbums = albums?.filter((folder) =>
-      folder.name.toLowerCase().includes(e.target.value)
-    );
-
-    if (!filteredAlbums) {
-      setSearchAlbums([]);
-    }
-
-    setSearchAlbums(filteredAlbums);
-  };
 
   return (
     <Dialog
@@ -81,43 +67,14 @@ export function AddToAlbumDialog({ image, onClose }: AddToAlbumProps) {
             Provide a name for your album where you want to move this image.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4 relative">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-left">
-              Album
-            </Label>
-            <Input
-              id="name"
-              onChange={handleChange}
-              value={albumName}
-              className="col-span-4 lowercase"
-              placeholder="album name"
-            />
-          </div>
-
-          {albumName && searchAlbums && searchAlbums?.length > 0 && (
-            <div className="absolute bg-slate-950 border rounded-md top-24 w-60 flex flex-col space-y-1 p-2 max-h-[250px] overflow-auto searchAlbums">
-              {searchAlbums?.map((folder) => (
-                <p
-                  className="p-2 rounded-sm hover:bg-slate-600 cursor-pointer flex items-center"
-                  onClick={() => {
-                    setAlbumName(folder.name);
-                    setSearchAlbums([]);
-                  }}
-                  key={folder.path}
-                >
-                  <span>
-                    <Folder className="w-4 h-4 mr-2" />
-                  </span>
-                  {folder.name}
-                </p>
-              ))}
-            </div>
-          )}
-        </div>
+        <SearchAlbums
+          albumName={albumName}
+          albums={albums}
+          setAlbumName={setAlbumName}
+        />
         <DialogFooter>
           <Button onClick={handleClick} type="submit">
-            Add
+            {isPending ? <Loader2 className="w-4 h-4 animate-spin" /> : "Add"}
           </Button>
         </DialogFooter>
       </DialogContent>
---
+++ src/components/search-albums.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Folder } from "lucide-react";
+import React, { ChangeEvent, SetStateAction, useState } from "react";
+import { FolderType } from "../../types";
+import { Input } from "./ui/input";
+
+type SearchAlbumsProps = {
+  albumName: string;
+  albums: FolderType[] | undefined;
+  setAlbumName: React.Dispatch<SetStateAction<string>>;
+};
+
+const SearchAlbums = ({
+  albumName,
+  albums,
+  setAlbumName,
+}: SearchAlbumsProps) => {
+  const [searchAlbums, setSearchAlbums] = useState<FolderType[]>();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setAlbumName(e.target.value);
+
+    const filteredAlbums = albums?.filter((folder) =>
+      folder.name.toLowerCase().includes(e.target.value)
+    );
+
+    if (!filteredAlbums) {
+      setSearchAlbums([]);
+    }
+
+    setSearchAlbums(filteredAlbums);
+  };
+
+  return (
+    <div className="relative">
+      <Input
+        id="name"
+        onChange={handleChange}
+        value={albumName}
+        className="col-span-4 lowercase"
+        placeholder="album name"
+      />
+      {searchAlbums && searchAlbums?.length > 0 && (
+        <div className="absolute bg-slate-950 border rounded-md top-12 w-60 flex flex-col space-y-1 p-2 max-h-[250px] overflow-auto searchAlbums">
+          {searchAlbums?.map((folder) => (
+            <p
+              className="p-2 rounded-sm hover:bg-slate-600 cursor-pointer flex items-center"
+              onClick={() => {
+                setAlbumName(folder.name);
+                setSearchAlbums([]);
+              }}
+              key={folder.path}
+            >
+              <span>
+                <Folder className="w-4 h-4 mr-2" />
+              </span>
+              {folder.name}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SearchAlbums;
