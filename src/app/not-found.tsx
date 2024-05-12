2db1b5c81251cb43cc1f36df109cbf36e6e33673
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-13 01:06:37 +0530

    feat: style the not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="w-full flex justify-center items-center min-h-screen flex-col">
       <h2 className="text-4xl font-bold">Not Found</h2>
-      <div className="mt-5 bg-slate-800 p-4 rounded-md text-slate-400 flex flex-col space-y-2 max-w-[400px]">
+      <div className="mt-5 bg-neutral-800 p-4 rounded-md text-neutral-400 flex flex-col space-y-2 max-w-[400px]">
         <p className="text-sm">Could not find requested resource</p>
         <Link href="/gallery">
           <Button variant={"default"} size={"sm"}>
