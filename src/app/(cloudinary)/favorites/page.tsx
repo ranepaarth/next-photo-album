042bec14daf4bea1407e81cb5701a539c82907a9
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-16 19:58:26 +0530

    feat: implement  image masonry skeleton
    This skeleton was implemented for the isPending state while fetching the favorite images within favorites page.

--- src/app/(cloudinary)/favorites/page.tsx
+++ src/app/(cloudinary)/favorites/page.tsx
@@ -2,13 +2,14 @@
 
 import { fetchFavoriteImages } from "@/actions/fetch-favorites";
 import FavoritesList from "@/app/(cloudinary)/favorites/fovirtes-list";
+import ImageMasonrySkeleton from "@/components/image-masonry-skeleton";
 import React, { useEffect, useState, useTransition } from "react";
 import { CloudinarySearchResults } from "../../../../types";
 
 const FavoritesPage = () => {
   const [results, setResults] = useState<CloudinarySearchResults>();
-
   const [isPending, startTransition] = useTransition();
+
   useEffect(() => {
     startTransition(async () => {
       const response = await fetchFavoriteImages();
@@ -17,13 +18,16 @@ const FavoritesPage = () => {
     });
   }, []);
 
+  let content;
+  if (isPending) content = <ImageMasonrySkeleton />;
+  else content = <FavoritesList initialResources={results?.resources} />;
+
   return (
     <section>
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold mb-8">Favorites</h1>
       </div>
-
-      <FavoritesList initialResources={results?.resources} />
+      {content}
     </section>
   );
 };
