8b266e768090cbdd4b142d309f1fde2e85ed5ae2
ranepaarth <2019paarth.rane@ves.ac.in>
2024-05-17 01:04:53 +0530

    🚧add cloud name in cloudinary search expression

--- src/actions/fetch-favorites.ts
+++ src/actions/fetch-favorites.ts
@@ -5,7 +5,9 @@ import { CloudinarySearchResults } from "../../types";
 
 export const fetchFavoriteImages = async () => {
   const result: CloudinarySearchResults = await cloudinary.v2.search
-    .expression("resource_type:image AND tags=favorite")
+    .expression(
+      `cloud_name:${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME} resource_type:image AND tags=favorite`
+    )
     .with_field("tags")
     .sort_by("last_updated.tags_updated_at", "desc")
     .max_results(30)
